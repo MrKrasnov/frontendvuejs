06b8ee8bf2026fc6750de2ded166b32ac2ec6b98
MrKrasnov <mr.krasnov.y@gmal.com>
2020-01-12 18:35:17 +0200

    V1.3
    Получен и выведен на сайт формат JSON

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,38 @@
 <template>
   <div id="app">
-    
+    <div v-for="post in posts" :key="post.id" class="post">
+      <div class="info">
+      {{post.id}}
+      </div>
+      <div>
+        <p>Name: {{post.name}}</p>
+        <p>Username: {{post.username}}</p>
+        <p>Email: {{post.email}}</p>
+      </div>
+      <div>
+      <ul>Adress:
+        <li>Street: {{post.address.street}}</li>
+        <li>Suite: {{post.address.suite}}</li>
+        <li>City: {{post.address.city}}</li>
+        <li>Zipcode: {{post.address.zipcode}}</li>
+        <li>Geo:
+            <p>lat: {{post.address.geo.lat}}</p>
+            <p>lng {{post.address.geo.lng}}</p>
+        </li>
+      </ul>
+      </div>
+      <div>
+      <p>Phone: {{post.phone}}</p>
+      <p>Website: {{post.website}}</p>
+      </div>
+      <div>
+      <ul> Company:
+        <li>Name: "{{post.company.name}}"</li>
+        <li>Motto: {{post.company.catchPhrase}}</li>
+        <li>Product: {{post.company.bs}}</li>
+      </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -27,12 +59,33 @@ export default {
 </script>
 
 <style>
+body{
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.post li p {
+  border: none;
+}
+p{
+  margin: 0;
+}
+.post{
+  padding-top: 20px;
+  display: flex;
+  justify-content: space-between;
+  color:#2c3e50;
+  font-size: 15px;
+}
+.info{
+  padding-left: 20px;
+}
+ul{
+  margin: 0;
 }
 </style>
