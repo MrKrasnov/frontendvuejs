5c32ffc1f9180b62bd43448a0ceadc2ce1d0e0bf
MrKrasnov <mr.krasnov.y@gmal.com>
2020-01-23 22:18:03 +0200

    V2.8
    computed в listchart компоненте

--- front-end/src/Listchart.vue
+++ front-end/src/Listchart.vue
@@ -35,16 +35,14 @@
         },
         computed: {
             labels: function() {
-                 
+                
                let array1 = [];
-            //    let f = this.postchart[1].userId;
+
+               // инициализация массива
                for (let index = 0; index < 100; index++) {
                    array1.push([this.postchart[index].userId,this.postchart[index].title]);
-               }
-               
-                // let array1 = [{name:1},{name:2}];
-                // let array = [];
-                 
+               } 
+
                  for (let ik = 0; ik < array1.length; ik++) {
                     let name_unique = array1[ik][0];
                     for (let j = 0; j <array1.length; j++) {
@@ -56,11 +54,14 @@
                     }
                 }
             }
+
             let obj = [];
+            // label: [1, 2, 3,]
             for (var i = 0; i < array1.length; i++) {
                 obj.push(array1[i][0]);
             }
             return obj;
+
             }
         },
         created: function () {
