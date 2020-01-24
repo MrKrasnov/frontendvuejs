cd31cf0e5cdd8e2e5647d8ae569598378f0fc35c
MrKrasnov <mr.krasnov.y@gmal.com>
2020-01-25 00:43:57 +0200

    V2.9
    Заверщение логической части ТЗ

--- front-end/src/Listchart.vue
+++ front-end/src/Listchart.vue
@@ -18,11 +18,12 @@
         data (){ 
             return {
                 postchart: [],
+                click: [],
                 // labels:
-                datasets: [{
-                    backgroundColor: '#f87979',
-                    data: [10, 10, 10 , 10, 10, 10 , 10, 10, 10, 10],
-                }],
+                // datasets: [{
+                //     backgroundColor: '#f87979',
+                //     data: [10, 10, 10 , 10, 10, 10 , 10, 10, 10, 10],
+                // }],
                 option: {
                     title: {
                         display: true,
@@ -36,32 +37,106 @@
         computed: {
             labels: function() {
                 
-               let array1 = [];
+               let localArray = [];
 
                // инициализация массива
                for (let index = 0; index < 100; index++) {
-                   array1.push([this.postchart[index].userId,this.postchart[index].title]);
+                   localArray.push([this.postchart[index].userId,this.postchart[index].title]);
                } 
+                 for (let ik = 0; ik < localArray.length; ik++) {
 
-                 for (let ik = 0; ik < array1.length; ik++) {
-                    let name_unique = array1[ik][0];
-                    for (let j = 0; j <array1.length; j++) {
-                        let name_not_unique =array1[j][0];
+                    let name_unique = localArray[ik][0];
+                    for (let j = 0; j <localArray.length; j++) {
+                        let name_not_unique =localArray[j][0];
+                    // значение повторяется
                     if(ik != j  && name_unique == name_not_unique){
-                        array1.splice(ik, 1);
+                        
+                        localArray.splice(ik, 1);
                         ik-=1;
                         break;
                     }
                 }
             }
 
-            let obj = [];
+            let result = [];
             // label: [1, 2, 3,]
-            for (var i = 0; i < array1.length; i++) {
-                obj.push(array1[i][0]);
+            for (var i = 0; i < localArray.length; i++) {
+                result.push(localArray[i][0]);
             }
-            return obj;
+            return result;
 
+            },
+            datasets: function (){
+                let array1 = [];
+
+               // инициализация массива
+               for (let index = 0; index < 100; index++) {
+                   array1.push([this.postchart[index].userId,this.postchart[index].title]);
+               } 
+               let not_unique = [];
+                // проверяем имя на повторность
+                 for (let ik = 0; ik < array1.length; ik++) {
+                     
+                    let name_unique = array1[ik][0];
+                    
+                    for (let j = 0; j <array1.length; j++) {
+                    // Берет такое же значение
+                        let name_not_unique =array1[j][0];
+                        
+                    // если значение повторяется
+                    if(ik != j  && name_unique == name_not_unique){
+                        not_unique.push(array1[j][0]);
+                        array1.splice(ik, 1);
+                        ik-=1;
+                        break;
+                    }
+
+                }
+            }
+            let result = [{
+                    backgroundColor: '#f87979',
+                    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                }];
+                
+                let data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                for(let user of not_unique){
+
+                    switch(user){
+                        case 1:
+                            data[0] += 1;
+                            break;
+                        case 2:
+                            data[1] += 1;
+                            break;
+                        case 3:
+                            data[2] += 1;
+                            break;
+                        case 4:
+                            data[3] += 1;
+                            break;
+                        case 5:
+                            data[4] += 1;
+                            break;
+                        case 6:
+                            data[5] += 1;
+                            break;
+                        case 7:
+                            data[6] += 1;
+                            break;
+                        case 8:
+                            data[7] += 1;
+                            break;
+                        case 9:
+                            data[8] += 1;
+                            break;
+                        case 10:
+                            data[9] += 1;
+                            break;
+                    }
+                }
+                
+                result[0].data = data;
+                return result;            
             }
         },
         created: function () {
